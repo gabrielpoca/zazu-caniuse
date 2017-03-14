93f32ac73c08063d444a348e48688b47b0ae793b
Gabriel Poça <gabrielpoca@gmail.com>
2017-03-14 15:10:10 +0000

    Add list of supported browsers to title
    Feature requested in
    https://github.com/gabrielpoca/zazu-caniuse/issues/1 .

--- src/data.js
+++ src/data.js
@@ -1,9 +1,57 @@
 const featuresData = require('caniuse-db/data.json').data
 
+const browsersInTitle = [
+  'ie',
+  'edge',
+  'firefox',
+  'chrome',
+  'safari',
+]
+
+const browserToKey = {
+  ie: 'IE',
+  edge: 'EDGE',
+  firefox: 'FF',
+  chrome: 'CH',
+  safari: 'S',
+}
+
+const firstBrowserVersionForFeature = (feature, browser) => {
+  return Object.keys(feature.stats[browser]).filter((version) => {
+    return feature.stats[browser][version].indexOf('y') !== -1
+  })[0]
+}
+
+const titleStatsForFeature = (feature) => {
+  return Object.keys(feature.stats)
+    .filter(browser => {
+      return browsersInTitle.indexOf(browser) !== -1
+    })
+    .reduce((list, browser) => {
+      const firstVersion = firstBrowserVersionForFeature(feature, browser)
+
+      if (firstVersion) {
+        list.push(`${browserToKey[browser]}:${firstVersion}+`)
+      }
+
+      return list
+    }, [])
+    .join(', ')
+}
+
 const features = Object.keys(featuresData)
-  .reduce((memo, feature) => {
-    featuresData[feature].slug = feature
-    memo.push(featuresData[feature])
+  .reduce((memo, featureSlug) => {
+    const feature = featuresData[featureSlug]
+
+    const titleStats = titleStatsForFeature(feature)
+
+    if (titleStats !== '') {
+      feature.title += ` [${titleStats}]`
+    }
+
+    feature.slug = featureSlug
+
+    memo.push(feature)
     return memo
   }, [])
 
